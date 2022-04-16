d7387e8990e7f86c8aeae3acc4da8c10d97911fa
pasha-minsk <78148273+pasha-minsk@users.noreply.github.com>
2022-04-16 23:51:15 +0300

    create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// современный режим
+"use strict"
+// alert(3+3+73);
+// alert("Сейчас будет ошибка");
+// [1, 2].forEach(alert);
+// console.log('Hello world!');
+// let message;
+// message="Hello";
+// console. log(message);
+// let message="Hello";
+// let message="Hello";
