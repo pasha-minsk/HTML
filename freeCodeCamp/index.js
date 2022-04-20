b9cfd62589d38e9c988372ab4c048c13d9092816
pasha-minsk <78148273+pasha-minsk@users.noreply.github.com>
2022-04-20 14:17:29 +0300

    create function myOutFit

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -70,3 +70,14 @@ functionWithArgs(3, 4)
 function timesFive(num) {
     return num * 5;
 }
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+    // Only change code below this line
+    let outerWear = 'sweater';
+    // Only change code above this line
+    return outerWear;
+}
+
+myOutfit();
