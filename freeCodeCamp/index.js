a52e75561cb7ff68180002bbee9b093e9ad4854c
pasha-minsk <78148273+pasha-minsk@users.noreply.github.com>
2022-04-22 23:32:31 +0300

    create function orderMyLogic

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -145,3 +145,16 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+
+function orderMyLogic(val) {
+    if (val < 10 && val >= 5) {
+        return "Less than 10";
+    } else if (val < 5) {
+        return "Less than 5";
+    } else {
+        return "Greater than or equal to 10";
+    }
+}
+
+orderMyLogic(7);
