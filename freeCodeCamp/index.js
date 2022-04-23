3c809af5ae65da6b09fbe81495cac08fc6ec5922
pasha-minsk <78148273+pasha-minsk@users.noreply.github.com>
2022-04-23 23:34:40 +0300

    function phoneticLookup with object

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -364,3 +364,23 @@ const myDog = {
 
 // Only change code below this line
 delete myDog.tails
+
+
+function phoneticLookup(val) {
+    let result = "";
+
+    // Only change code below this line
+    let lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    }
+    result = lookup[val]
+        // Only change code above this line
+    return result;
+}
+
+phoneticLookup("charlie");
