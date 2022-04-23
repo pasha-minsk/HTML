8fa51ef1bb9652853bfe6f28825a34a886479e81
pasha-minsk <78148273+pasha-minsk@users.noreply.github.com>
2022-04-24 00:07:33 +0300

    function checkObj

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -384,3 +384,14 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+
+    if (obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp];
+    } else {
+        return "Not Found";
+    }
+    // Only change code above this line
+}
