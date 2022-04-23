bc9858e69604f29e52152d4c11e9b02a303c2bd8
pasha-minsk <78148273+pasha-minsk@users.noreply.github.com>
2022-04-23 22:41:34 +0300

    using bracket notation

--- freeCodeCamp/index.js
+++ freeCodeCamp/index.js
@@ -342,3 +342,13 @@ const testObj = {
 };
 const hatValue = testObj.hat; // Change this line
 const shirtValue = testObj.shirt; // Change this line
+
+
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+const entreeValue = testObj["an entree"]; // Change this line
+const drinkValue = testObj["the drink"];
